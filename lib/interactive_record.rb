7defffb04f3ad7e4d46b62affd12e894e4c93d91
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-02-18 04:30:16 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -55,7 +55,11 @@ class InteractiveRecord
   
   def self.find_by(hash)
     sql = <<-SQL
-     SELECT * FROM #{self.table_name} WHERE #{hash.keys[0].to_s} = '#{hash.values[0].to_s}'"
+     "SELECT * FROM #{self.table_name} WHERE #{hash.keys[0].to_s} = '#{hash.values[0].to_s}'"
+    DB[:conn].execute(sql)
+   end 
+   
+
      
   
 
